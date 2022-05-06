e42a98c625f7a4a3489b0f308f25064669684af3
Titouan-Gacougnolle <tgacougnolle@gmail.com>
2022-05-06 17:37:20 +0200

    cinquième commit

--- projetmoto/applimoto/models.py
+++ projetmoto/applimoto/models.py
@@ -9,7 +9,7 @@ class Ecurie(models.Model):
     date_creation = models.DateField(blank=True, null=True)
 
     def __str__(self):
-        texte = f"Voici la team {self.nom} de la marque {self.marque} gérée par {self.team_manager}"
+        texte = f"{self.nom}"
         return texte
 
     def dico(self):
@@ -18,15 +18,17 @@ class Ecurie(models.Model):
 
 
 class Pilote(models.Model):
+    ecurie = models.ForeignKey("Ecurie", on_delete=models.CASCADE, default=None)
     nom = models.CharField(max_length=20)
     prenom = models.CharField(max_length=20)
     age = models.IntegerField(blank=False)
 
+
     #ecurie = models.ForeignKey(Ecurie, default=None)
 
     def __str__(self):
-        textepilote = f"Voici le pilote {self.nom} {self.prenom}"
+        textepilote = f"{self.prenom} {self.nom}"
         return textepilote
 
     def dico(self):
-        return {'nom': self.nom, 'prenom': self.prenom, 'age': self.age}
+        return {'nom': self.nom, 'prenom': self.prenom, 'age': self.age, 'ecurie': self.ecurie.nom}
